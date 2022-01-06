babc47ff5d68105f89dacc9d6a1a5716d9edcd93
Julio Ucharima <julio_ucharima@usmp.pe>
2022-01-06 17:12:51 -0500

    conversiones

--- js/calculator.js
+++ js/calculator.js
@@ -1,23 +1,23 @@
-const API_URL =
-  "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=bitcoin%2C%20ethereum%2C%20dogecoin%2C%20matic-network%2C%20binancecoin&order=market_cap_desc&per_page=100&page=1&sparkline=false";
-const HTMLresponse = document.querySelector("#coins");
-const ul = document.createElement("ul");
+// const API_URL =
+//   "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=bitcoin%2C%20ethereum%2C%20dogecoin%2C%20matic-network%2C%20binancecoin&order=market_cap_desc&per_page=100&page=1&sparkline=false";
+// const HTMLresponse = document.querySelector("#coins");
+// const ul = document.createElement("ul");
 
-fetch(`${API_URL}`)
-  .then((response) => response.json())
-  .then((coins) => {
-    coins.forEach((coin) => {
-      let elem = document.createElement("li");
-      elem.appendChild(
-        document.createTextNode(
-          `${coin.name} - $${coin.current_price} icono: ${coin.image}`
-        )
-      );
-      ul.appendChild(elem);
-    });
+// fetch(`${API_URL}`)
+//   .then((response) => response.json())
+//   .then((coins) => {
+//     coins.forEach((coin) => {
+//       let elem = document.createElement("li");
+//       elem.appendChild(
+//         document.createTextNode(
+//           `${coin.name} - $${coin.current_price} icono: ${coin.image}`
+//         )
+//       );
+//       ul.appendChild(elem);
+//     });
 
-    HTMLresponse.appendChild(ul);
-  });
+//     HTMLresponse.appendChild(ul);
+//   });
 
 /* ----- Metodo alternativo para manipular DOM/ Hacer una petición a la API de  y mostrar los resultados en una lista ----- */
 // fetch(`${API_URL}`)
@@ -91,29 +91,29 @@ function blockAmount() {
   }
 }
 
-function showHashpowerHelp() {
-  var x = document.querySelector(
-    "body > div.row.no-gutters.hashinput > div > div:nth-child(1) > img"
-  );
-  if (x.classList.contains("hide")) {
-    x.classList.remove("hide");
-  } else {
-    x.classList.add("hide");
-  }
-}
+// function showHashpowerHelp() {
+//   var x = document.querySelector(
+//     "body > div.row.no-gutters.hashinput > div > div:nth-child(1) > img"
+//   );
+//   if (x.classList.contains("hide")) {
+//     x.classList.remove("hide");
+//   } else {
+//     x.classList.add("hide");
+//   }
+// }
 
-function supportMeToggle() {
-  var x = document.querySelector("body > div.supportMe");
-  if (x.classList.contains("hidden")) {
-    x.classList.add("animate__animate", "animate__fadeInLeft");
-    x.classList.remove("hidden");
-    console.log("revealed SupportMe");
-    // x.classList.add("animate__animated", "animate__bounceOutLeft");
-  } else {
-    x.classList.add("hidden");
-    console.log("Hide SupportMe");
-  }
-}
+// function supportMeToggle() {
+//   var x = document.querySelector("body > div.supportMe");
+//   if (x.classList.contains("hidden")) {
+//     x.classList.add("animate__animate", "animate__fadeInLeft");
+//     x.classList.remove("hidden");
+//     console.log("revealed SupportMe");
+//     // x.classList.add("animate__animated", "animate__bounceOutLeft");
+//   } else {
+//     x.classList.add("hidden");
+//     console.log("Hide SupportMe");
+//   }
+// }
 
 function calculateGoalPower() {
   console.log("Calculate Begin");
@@ -249,4 +249,101 @@ function calculateGoalPower() {
         (rltResult * 30).toFixed(4) + " Rollertoken";
       break;
   }
+}
+
+// https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=bitcoin&order=market_cap_desc&per_page=100&page=1&sparkline=false
+
+var xhReq = new XMLHttpRequest();
+xhReq.open("GET", "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=bitcoin&order=market_cap_desc&per_page=100&page=1&sparkline=false", false)
+xhReq.send(null);
+var bitcoinData = JSON.parse(xhReq.responseText);
+
+console.log(bitcoinData);
+
+function calculateBTC(){
+  var satoshi = parseFloat(document.getElementById("satoshi-value").value);
+  var btc = satoshi/100000000;
+
+  console.log(satoshi);
+
+  console.log(btc);
+
+  document.getElementById("btc-resultado").value = btc.toFixed(8);
+}
+
+function calculated$(){
+  var crypto = parseFloat(document.getElementById("btc-value").value);
+
+  var btc_dolar = crypto * 46132.10;
+  var btc_euro = crypto * 41293.76;
+
+  var eth_dolar = crypto * 3814.51;
+  var eth_euro = crypto * 3386.72;
+
+  var bnb_dolar = crypto * 511.14;
+  var bnb_euro = crypto * 452.92;
+
+  var doge_dolar = crypto * 0.17;
+  var doge_euro = crypto * 0.12;
+
+  var matic_dolar = crypto * 2.42;
+  var matic_euro = crypto * 2.14;
+
+  switch (document.getElementById("crypto-type").selectedIndex){
+    case 0:
+      switch (document.getElementById("coin-type").selectedIndex) {
+        case 0:
+            document.getElementById("coin-resultado").value = btc_dolar.toFixed(2);
+          break;
+        default:
+            document.getElementById("coin-resultado").value = btc_euro.toFixed(2);
+          break;
+      }
+    break;
+    case 1:
+      switch (document.getElementById("coin-type").selectedIndex) {
+        case 0:
+            document.getElementById("coin-resultado").value = eth_dolar.toFixed(2);
+          break;
+        default:
+            document.getElementById("coin-resultado").value = eth_euro.toFixed(2);
+          break;
+      }
+    break;
+    case 2:
+      switch (document.getElementById("coin-type").selectedIndex) {
+        case 0:
+            document.getElementById("coin-resultado").value = bnb_dolar.toFixed(2);
+          break;
+        default:
+            document.getElementById("coin-resultado").value = bnb_euro.toFixed(2);
+          break;
+      }
+    break;
+    case 3:
+      switch (document.getElementById("coin-type").selectedIndex) {
+        case 0:
+            document.getElementById("coin-resultado").value = doge_dolar.toFixed(2);
+          break;
+        default:
+            document.getElementById("coin-resultado").value = doge_euro.toFixed(2);
+          break;
+      }
+    break;
+    default:
+      switch (document.getElementById("coin-type").selectedIndex) {
+        case 0:
+            document.getElementById("coin-resultado").value = matic_dolar.toFixed(2);
+          break;
+        default:
+            document.getElementById("coin-resultado").value = matic_euro.toFixed(2);
+          break;
+      }
+    break;
+  }
+
+  console.log(crypto);
+  console.log(btc_dolar);
+
+  // document.getElementById("coin-resultado").value = dolar.toFixed(2);
 }
